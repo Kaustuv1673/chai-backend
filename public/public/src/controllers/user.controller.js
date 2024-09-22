203059239e21393afa09b840c3b62dbb099c63fb
Kaustuv Mani Ojha <kaustuvmaniojha@gmail.com>
2024-09-22 10:56:10 +0530

    Fix all register controller bugs

--- public/public/src/controllers/user.controller.js
+++ public/public/src/controllers/user.controller.js
@@ -22,7 +22,7 @@ const registerUser=asynceHandler(async(req,res)=>{
         throw new ApiError(400,"All fields are required")
     }
 
-    const existedUser=User.findOne({
+    const existedUser=await User.findOne({
         $or:[{ username },{ email }]
     })
 
@@ -31,7 +31,11 @@ const registerUser=asynceHandler(async(req,res)=>{
     }
 
     const avatarLocalPath=req.files?.avatar[0]?.path;
-    const coverImageLocalPath=req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath=req.files?.coverImage[0]?.path;
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.covereImage.length>0){
+        coverImageLocalPath=req.files.coverImage[0].path 
+    }
    
     if(!avatarLocalPath){
         throw new ApiError(400,"Avatar file is required")
